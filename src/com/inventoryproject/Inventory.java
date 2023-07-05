4397de494f010cb511d18b4475b5a1960205cb20
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-07-04 18:15:37 -0700

    Initial commit

---
+++ src/com/inventoryproject/Inventory.java
@@ -0,0 +1,11 @@
+package com.inventoryproject;
+
+public class Inventory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Comment");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author srim
+ *
+ */
+module InventoryChecker {
+}
