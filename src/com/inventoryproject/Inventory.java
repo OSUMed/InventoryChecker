0aada06d4ad1830c1ce914cca297188442525e91
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-07-04 18:39:07 -0700

    feat added Product Class

--- src/com/inventoryproject/Inventory.java
+++ src/com/inventoryproject/Inventory.java
@@ -1,12 +1,13 @@
 package com.inventoryproject;
-
+import com.inventoryproject.services.*;
 public class Inventory {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First Comment");
 		System.out.println("Second Comment");
-
+		Product firstProduct = new Product(0, "My Book", 10, 1);
+		System.out.println(firstProduct);
 	}
 
 }
