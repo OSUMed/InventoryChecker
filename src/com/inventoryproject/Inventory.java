46d9bef08d811ce2d3ff02e7c098dba40c359da5
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-07-04 18:21:18 -0700

    Initial Commit

--- src/com/inventoryproject/Inventory.java
+++ src/com/inventoryproject/Inventory.java
@@ -5,6 +5,7 @@ public class Inventory {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First Comment");
+		System.out.println("Second Comment");
 
 	}
 
