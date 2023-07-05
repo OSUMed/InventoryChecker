6d679514f7f8e81fd042e0596891de3654f50bba
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-07-04 20:34:41 -0700

    feat: Create list of objects from file data

--- src/com/inventoryproject/Inventory.java
+++ src/com/inventoryproject/Inventory.java
@@ -16,20 +16,38 @@ public class Inventory {
 		
 		// Change input data String -> Objects:
 		// eg: Product firstProduct = new Product(0, "My Book", 10, 1);
+		ArrayList<Product> productObjects = new ArrayList<>();
 		boolean isFirstLine = true;
 		for (ArrayList<String> line : lines) {
-			for (String property : line) {
+			for (String properties : line) {
+				
+				// Skip Headers:
 				if (isFirstLine) {
 					isFirstLine = false;
 					continue;
 				}
-				System.out.println(property);
+				
+				// Create List of strings and change to format ArrayList:
+				String[] propertiesList = properties.split(",");
+				ArrayList<String> propertiesArrayList = new ArrayList<>();
+				for (String item: propertiesList)
+					propertiesArrayList.add(item);
+				
+		        // Access elements by index using get
+				int id = Integer.parseInt(propertiesArrayList.get(0));
+				String name = propertiesArrayList.get(1);
+				int quantity = Integer.parseInt(propertiesArrayList.get(2));
+		        float price = Float.parseFloat(propertiesArrayList.get(3));
+		        
+		        // Create new Object and add to list:
+		        Product product = new Product(id, name, quantity, price)
+				productObjects.add(product);
+		       
 			}
 		}
+		System.out.println(productObjects);
 		
-		
-		
-		// Collect necessary information:
+		// Collect necessary information by iterating through objects:
 		
 		
 		// Export relevant data into output file:
