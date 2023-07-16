42961d066fa526c8b4772fed41ef07c8a524b6a9
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-07-16 13:01:59 -0700

    Added documentation

--- src/com/inventoryproject/services/FileService.java
+++ src/com/inventoryproject/services/FileService.java
@@ -13,24 +13,37 @@ public class FileService {
 		
 	}
 	
-	// Singleton so only one instance made:
+	/**
+	 * Singleton so only one instance is created.
+	 * @return The instance of FileService.
+	 */
 	public static FileService getInstance() {
 		if (fileService == null) {
 			fileService = new FileService();
 		}
 		return fileService;		
 	}
-	
-	// Reads files and inserts its contents into a data structure:
+
+	/**
+	Reads the contents of a file and inserts them into a data structure.
+	@param path The path of the file to be read.
+	@return An ArrayList of ArrayLists, where each inner ArrayList represents a line from the file.
+	@throws IOException If an error occurs while reading the file.
+	*/
 	public ArrayList<ArrayList<String>> getFileContent(Path path) throws IOException{
+		
+		// Read the file and collect its contents:
 		ArrayList<String> data = (ArrayList<String>) Files.readAllLines(path);
+		
+		// Create a data structure to store each line from the file:
 		ArrayList<ArrayList<String>> lines = new ArrayList<ArrayList<String>>();
+		
+		// Iterate over the data and add each line to the lines data structure:
 		for (int i = 0; i < data.size(); i++) {
 			ArrayList<String> line = new ArrayList<>();
 			line.add(data.get(i));	
 			lines.add(line);
 		}
-		
 		return lines;
 
 	}
